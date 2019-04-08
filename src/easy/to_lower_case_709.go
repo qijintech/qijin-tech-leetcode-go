e9496945bface35c169ae8b543fc6faf7122d208
fireagainsmile <w952577382@126.com>
2019-04-08 17:19:31 +0800

    rewrite this solution other than system lib

--- src/easy/to_lower_case_709.go
+++ src/easy/to_lower_case_709.go
@@ -1,3 +1,12 @@
 func toLowerCase(str string) string {
-    return strings.ToLower(str)
+	b := make([]rune,len(str))
+	offset := 'a' - 'A'
+	for index, value := range str {
+		if value >= 'A' && str[index] <='Z'{
+			b[index] = value + offset
+		}else {
+			b[index] = value
+		}
+	}
+	return string(b)
 }
